a2acad0487827474c82034aa82c56e3226be5b28
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-03-05 15:25:29 -0800

    Feat: assign min and max to properties
    Directly assigned values to min and max properties,
    NOT returning a function called min() that returns the min
    
    Use Array.reduce() to go through array

--- analyze.js
+++ analyze.js
@@ -1,6 +1,15 @@
 const analyzeArray = function(array){
+    const getGreater = function(num1,num2){
+        return Math.max(num1,num2);
+    }
+    const getLesser = function(num1,num2){
+        return Math.min(num1,num2);
+    }
 
-    return {};
+    return {
+        max: array.reduce(getGreater),
+        min: array.reduce(getLesser),
+    };
 }
 
 export {analyzeArray};
