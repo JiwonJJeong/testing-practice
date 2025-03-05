00ec4246425c32dbd5dd2ecbc5866ebaa4a9ec9a
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-03-05 14:52:01 -0800

    Feat: preserve capitalization

--- cipher.js
+++ cipher.js
@@ -2,10 +2,19 @@ const caesarCipher = function(string, shiftNum){
     const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     let newString = "";
     for (const char of string){
-        let newIndex = (ALPHABET.indexOf(char)+shiftNum)%26;
-        newString += ALPHABET.at(newIndex);
+        if (isUpperCase(char)){
+            let newIndex = (ALPHABET.indexOf(char.toLowerCase())+shiftNum)%26;
+            newString += ALPHABET.at(newIndex).toUpperCase();
+        } else{
+            let newIndex = (ALPHABET.indexOf(char)+shiftNum)%26;
+            newString += ALPHABET.at(newIndex);
+        }
     }
     return newString;
+}
+
+const isUpperCase = function(char){
+    return char === char.toUpperCase();
 }
 
 export {caesarCipher};
