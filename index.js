f1e58fbb5261e49eb38c798e3e286c846e5688d1
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-03-05 13:49:04 -0800

    Feat: reverse string

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const capitalize = function(string){
     return string.slice(0,1).toUpperCase() + string.slice(1,string.length);
 }
 
-export {capitalize};
+const reverseString = function(string){
+    let charArray = string.split("");
+    let reverseString = "";
+    while (charArray.length >0){
+        reverseString += charArray.pop();
+    }
+    return reverseString;
+}
+
+export {capitalize, reverseString};
