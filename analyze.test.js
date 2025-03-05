c3a7b895ac0ca204828a0c7959801912a3034cef
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-03-05 15:20:49 -0800

    Feat: test min and max

--- analyze.test.js
+++ analyze.test.js
@@ -3,3 +3,22 @@ import {analyzeArray} from "./analyze.js";
 test("returns object", () => {
     expect(typeof(analyzeArray([1]))).toBe("object");
 })
+
+const obj1 = analyzeArray([1,8,3,4,2,6]);
+const obj2 = analyzeArray([2,4,2,4]);
+
+test("min property is minimum for obj1", () =>{
+    expect(obj1.min).toBe(1);
+})
+
+test("min property is minimum for obj2", () =>{
+    expect(obj2.min).toBe(2)
+})
+
+test("max property is maximum for obj1", () =>{
+    expect(obj1.max).toBe(8);
+})
+
+test("max property is maximum for obj2", () =>{
+    expect(obj2.max).toBe(4);
+})
