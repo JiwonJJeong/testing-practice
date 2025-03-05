ae67d5d181e62f15ca609c4b76811a7b70ada289
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-03-05 13:45:00 -0800

    Feat: test string reversal

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import {capitalize} from "./index.js";
+import {capitalize, reverseString} from "./index.js";
 
 test("capitalizes lower case 1", () => {
     expect(capitalize("hello")).toBe("Hello");
@@ -8,3 +8,10 @@ test("capitalizes lower case 2", () => {
     expect(capitalize("world")).toBe("World");
 });
 
+test("reverses hello world", () => {
+    expect(reverseString("hello world").toBe("dlrow olleh"));
+});
+
+test("reverses hello", () => {
+    expect(reverseString("hello").toBe("olleh"));
+});
